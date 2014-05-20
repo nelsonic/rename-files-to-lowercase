489234865272f802bec1eb5b4636b2357cafcef4
nelsonic <nodecoder@gmail.com>
2014-05-20 17:22:26 +0100

    tidy up indentation

--- script.js
+++ script.js
@@ -6,26 +6,26 @@ var testfilesdir = 'testfiles',
 var ncp = require('ncp').ncp; //https://github.com/AvianFlu/ncp
 // copy the testfiles directory to renamed:
 ncp(testfilesdir, renameddir, function (err) {
- if (err) {
-   return console.error(err);
- }
- console.log('done!');
+	if (err) {
+	return console.error(err);
+	}
+ 	console.log('directory copied');
 
- // walk through all the files in "testdirectory"
- fs.readdir(renameddir, function(err,files){
- 	if(err) {
- 		console.log('ERROR:',err);
- 	}
+ 	// walk through all the files in "testdirectory"
+	fs.readdir(renameddir, function(err,files){
+		if(err) {
+			console.log('ERROR:',err);
+		}
 
- 	var dir = __dirname +'/' + renameddir +'/';
- 	for (var i = files.length - 1; i >= 0; i--) {
- 		console.log('File:',files[i]);
- 		if(typeof files[i] !== 'undefined') {
- 			var file = files[i];
- 			// rename each file in the "renamed" dir toLowerCase:
- 			fs.renameSync(dir+file, dir+file.toLowerCase())
+		var dir = __dirname +'/' + renameddir +'/';
+		for (var i = files.length - 1; i >= 0; i--) {
+			console.log('File:',files[i]);
+			if(typeof files[i] !== 'undefined') {
+				var file = files[i];
+				// rename each file in the "renamed" dir toLowerCase:
+				fs.renameSync(dir+file, dir+file.toLowerCase())
 			console.log(file +' >> '+file.toLowerCase());
- 		}
- 	};
- })
+			}
+		};
+	})
 });
