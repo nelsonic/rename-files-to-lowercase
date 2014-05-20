297d0febe77f86d1ef97cf482245b4fa8792a36c
nelsonic <nodecoder@gmail.com>
2014-05-20 17:10:12 +0100

    first version working

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ rename-files-to-lowercase
 =========================
 
 Just a quick node script to rename all files in a directory to lower-case.
+
+## Why?
+
+Sometimes you will get files which have **strAngeNames.jpg** 
+but you need them to be **strangenames.jpg**.
+
+## What?
+
+A quick node script to transform the names of files.
+
+## How?
+
+Using Node's File System API: http://nodejs.org/api/fs.html
+to read a directory of files and re-name them to lower-case.
+
+Uses https://www.npmjs.org/package/ncp to copy source files to new dir first.
---
+++ script.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+var testfilesdir = 'testfiles',
+	renameddir   = 'renamed';
+
+var ncp = require('ncp').ncp; //https://github.com/AvianFlu/ncp
+// copy the testfiles directory to renamed:
+ncp(testfilesdir, renameddir, function (err) {
+ if (err) {
+   return console.error(err);
+ }
+ console.log('done!');
+
+ // walk through all the files in "testdirectory"
+ fs.readdir(renameddir, function(err,files){
+ 	if(err) {
+ 		console.log('ERROR:',err);
+ 	}
+
+ 	var dir = __dirname +'/' + renameddir +'/';
+ 	for (var i = files.length - 1; i >= 0; i--) {
+ 		console.log('File:',files[i]);
+ 		if(typeof files[i] !== 'undefined') {
+ 			var file = files[i];
+ 			// rename each file in the "renamed" dir toLowerCase:
+ 			fs.renameSync(dir+file, dir+file.toLowerCase())
+			console.log(file +' >> '+file.toLowerCase());
+ 		}
+ 	};
+ })
+});
